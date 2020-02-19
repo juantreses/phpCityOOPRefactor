46050a1e168d722b9444d2ce0b3b5829b44f5a80
alexandervdbroeck <alexander.vdbroeck@gmail.com>
2020-02-19 10:23:04 +0100

    Week.php is done: ----------------- the TemporaryPrintWeekTask.php needs to move to viewServices!

--- Service/TemporaryPrintWeekTask.php
+++ Service/TemporaryPrintWeekTask.php
@@ -13,29 +13,36 @@ class TemporaryPrintWeekTask
 
     public function printWeek($week, $year)
     {
-        $tasklist = "";
+        // correction of the week //
+        if( isset($_GET['week']) AND $week < 10 ) { $week = '0' . $week; }
+        if ($week > 52)
+        {
+            $year++;
+            $week = 1;
+        }
+        elseif ($week < 1)
+        {
+            $year--;
+            $week = 52;
+        }
+
+        // getting 7 rows of data (day, date and tasks)
         for( $day=1; $day <= 7; $day++ )
         {
             $d = strtotime($year . "W" . $week . $day);
-            $sqldate = date("Y-m-d", $d);
-            $data = $this->databaseService->getData( "SELECT taa_omschr FROM taak WHERE taa_datum = '".$sqldate."'" );
+            $dataReplaceContent[$day - 1]['day'] = date("l", $d);
+            $dataReplaceContent[$day - 1]['date'] = date("d/m/Y", $d);
+            $data = $this->databaseService->getData( "SELECT taa_omschr FROM taak WHERE taa_datum = '".date("Y-m-d", $d)."'" );
+            $dataReplaceContent[$day -1]['tasks']= ReplaceContent($data,LoadTemplate("week_tasks"));
 
-            $taken = array();
-            foreach( $data as $row )
-            {
-                $taken[] = $row['taa_omschr'];
-            }
-            $takenlijst = "<ul><li>" . implode( "</li><li>" , $taken ) . "</li></ul>";
-
-            $tableRow  = "<tr><td>". date("l", $d). "</td><td>". date("d/m/Y", $d). "</td><td>". $takenlijst . "</td></tr>";
-            $tasklist .= $tableRow;
         }
+        // get this data on the week.php page end replace the new links to previous and next week.
 
-        $link_vorige = "week.php?week=" . ($week == 1 ? 52 : $week - 1 ) . "&year=" . ($week == 1 ? $year - 1 : $year);
-        $link_volgende = "week.php?week=" . ($week == 52 ? 1 : $week + 1 ) . "&year=" . ($week == 52 ? $year + 1 : $year);
-        $tasklist .= "</table><a href=$link_vorige>Vorige Week</a>&nbsp<br><a href=$link_volgende>Volgende Week</a>&nbsp";
-        $replacecontent['week'] = $tasklist;
-        print ReplaceContentOneRow($replacecontent,LoadTemplate("week"));
+        $dataReplaceOneRow['tableContent'] =  ReplaceContent($dataReplaceContent,LoadTemplate("week_table_row"));
+        $dataReplaceOneRow['link_previous_week'] = "week.php?week=" . ($week == 1 ? 52 : $week - 1 ) . "&year=" . ($week == 1 ? $year - 1 : $year);
+        $dataReplaceOneRow['link_next_week'] = "week.php?week=" . ($week == 1 ? 52 : $week + 1 ) . "&year=" . ($week == 1 ? $year + 1 : $year);
+        print ReplaceContentOneRow($dataReplaceOneRow,LoadTemplate("week_table"));
+        return $newDate= array($week,$year);
 
 
     }
--- week.php
+++ week.php
@@ -6,34 +6,26 @@ BasicHead($css);
 $MS->ShowMessages();
 ?>
     <body>
-
     <div class="jumbotron text-center">
         <h1>Weekoverzicht</h1>
     </div>
     <?php
-            PrintNavBar();
+    PrintNavBar();
 
+
+            // Get the year and week
             $year = (isset($_GET['year'])) ? $_GET['year'] : date("Y");
             $week = (isset($_GET['week'])) ? $_GET['week'] : date("W");
-            if ($week > 52)
-            {
-                $year++;
-                $week = 1;
-            }
-            elseif ($week < 1)
-            {
-                $year--;
-                $week = 52;
-            }
-            if( isset($_GET['week']) AND $week < 10 ) { $week = '0' . $week; }
-
 
             $temporaryPrintWeekTask = $container->getTemporaryPrintWeekTask();
-            $temporaryPrintWeekTask->printWeek($week,$year);
+            // print the week and return the new date(link updated in class)
+            $newdate = $temporaryPrintWeekTask->printWeek($week,$year);
+            $week = $newdate[0];
+            $year = $newdate[1];
 
             ?>
 
-        </div>
+
     </div>
     </body>
 </html>
